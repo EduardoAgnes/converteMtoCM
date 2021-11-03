7c2580416ebc977313dbb45c31f90d7a3766c626
EduardoAgnes <93295261+EduardoAgnes@users.noreply.github.com>
2021-11-03 20:58:08 -0300

    Add files via upload

---
+++ converte.cpp
@@ -0,0 +1,21 @@
+/*
+Nome: Eduardo Agnes
+Implemente um sistema que receba qualquer numero inteiro de 0 a 9, e mostre na tela a tabuada do numero digitado
+
+*/
+
+#include <stdio.h>
+int main ()
+{
+   dev valor;
+   // Entrada - Sttart
+       printf("Digite um numero de 0 a 9 para saber a tabuada: \n");
+    scanf("%f",&valor
+    
+    
+    
+    
+    
+    
+	return 0;
+}
